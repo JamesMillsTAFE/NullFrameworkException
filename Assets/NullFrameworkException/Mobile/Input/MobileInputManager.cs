2b0c53d64993a32bb0b6f4d83a18f951c37e8919
James Mills <jmills@tunacornstudios.com>
2021-08-31 14:33:34 +1000

    Joystick improvements

--- Assets/NullFrameworkException/Mobile/Input/MobileInputManager.cs
+++ Assets/NullFrameworkException/Mobile/Input/MobileInputManager.cs
@@ -1,10 +1,15 @@
+using UnityEngine;
+
 namespace NullFrameworkException.Mobile.InputHandling
 {
     public class MobileInputManager : MonoSingleton<MobileInputManager>
     {
         public JoystickInputHandler joystick;
         public SwipeInputHandler swiping;
-        
+
+        /// <summary> Attempt to get the axis of the joystick attached to the system. </summary>
+        public static Vector2 GetJoystickAxis() => Instance.joystick != null ? Instance.joystick.Axis : Vector2.zero;
+
         // Start is called before the first frame update
         private void Start()
         {
